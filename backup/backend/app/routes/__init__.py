299c3ff562b0ebbce51f0b5a62ad7cf580622bd1
Emac56 <emacvicente8@gmail.com>
2026-05-28 16:52:58 +0800

    Delete unnessary files remain system architecture

--- backup/backend/app/routes/__init__.py
+++
@@ -1,3 +0,0 @@
-# app/routes/__init__.py
-# This file makes the 'routes' folder a Python package.
-# It can stay empty — its presence is what matters.
--- backup/backend/app/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-# app/utils/__init__.py
-# Makes 'utils' a Python package.
-# Import decorators here for convenience.
-
-from app.utils.decorators import admin_required, role_required
-
-__all__ = ['admin_required', 'role_required']
